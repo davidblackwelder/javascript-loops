3793139e0311d8f3c6de012c64dac62ea3e883cd
David Blackwelder <dblackw2@gmail.com>
2018-07-22 00:43:08 -0400

    Updated with Fix The Error programming quiz

---
+++ fix-the-error.js
@@ -0,0 +1,19 @@
+/*
+ * Programming Quiz: Fix the Error 1 (4-5)
+ */
+
+
+/*
+
+fix the for loop so that it prints the numbers 5 through 9
+
+for (x < 10; x++) {
+   console.log(x);
+}
+
+*/
+
+// fixed version
+for (var x = 5; x < 10; x++) {
+    console.log(x);
+}
